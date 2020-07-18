da30de01209004f5c9124fb544cc570f1522d957
Aqib_Iqbal <aqibiqbal48@gmail.com>
2020-07-18 19:51:26 +0500

    Update Product.js

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import LogoAndNavigation from '../Home/LogoAndNavigation';
 import Card from '../Home/Card';
-import data from '../../data.json';
 
 const Product = () => {
     return (
         <div>
             <LogoAndNavigation activeProduct="product"/>
-            <Card dataForPro={data}/>
+            <Card dataForPro="dataForProductx"/>
         </div>
     )
 }
