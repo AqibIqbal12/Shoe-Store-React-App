0d1d42b2d888787619840f6db5f9a8c255fd3e06
Aqib_Iqbal <aqibiqbal48@gmail.com>
2020-07-18 19:51:40 +0500

    Update DataReducer.js

--- src/context/DataReducer.js
+++ src/context/DataReducer.js
@@ -3,23 +3,47 @@ export default (state, action) => {
     switch (action.type) {
 
         case 'ADD_TO_CART': {
-            //return { cart: [action.payload, ...state.cart]}
+            return { cart: [action.payload, ...state.cart] };
+        }
+
+        case 'DECREASE_QTY': {
             let cart = [];
-            if (state.cart.some((prod) => prod.id === action.payload.id)) {
+            if (state.cart.some((prod) => prod.id === action.payload)) {
                 cart = state.cart.map((item) => {
                     //console.log(item)
-                    if (item.id === action.payload.id) {
-                        item.qty++;
+                    if (item.id === action.payload) {
+                        item.qty === 1 ? item.qty = 1 : item.qty--;
+                        item.subTotal === item.price ? item.itemLeft += 0  : item.itemLeft++;
+                        //item.itemLeft++;
                         item.subTotal = item.price * item.qty;
                     }
                     return item;
                 });
 
-            } else {
-                cart = [action.payload, ...state.cart];
             }
-            return { cart };
+            return { cart: cart };
         }
+
+        case 'INCREASE_QTY': {
+            let cart = [];
+            if (state.cart.some((prod) => prod.id === action.payload)) {
+
+                cart = state.cart.map((item) => {
+                    // console.log(item)
+                    if (item.id === action.payload) {
+                        item.qty++;
+                        item.itemLeft--;
+                        item.subTotal = item.price * item.qty;
+                    }
+                    return item;
+                });
+
+            }
+            return { cart: cart };
+
+        }
+
+
 
         case 'DELETE_CART_ITEM': {
             return { cart: state.cart.filter(item => item.id !== action.payload) }
