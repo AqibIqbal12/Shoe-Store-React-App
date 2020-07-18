3326e56fe1d7c148f2f28f9649918dde7972c130
Aqib_Iqbal <aqibiqbal48@gmail.com>
2020-07-18 19:50:37 +0500

    Update Cart.js

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,29 +2,26 @@ import React, { useContext } from 'react';
 import { Container, Row, Col, Table } from 'react-bootstrap';
 import LogoAndNavigation from './Home/LogoAndNavigation';
 import { CartContext } from '../context/CartContext'
-
+// import Style from './Cart.module.css'
 export const Cart = () => {
 
-    const { cart, deleteCartItem } = useContext(CartContext);
+    const { cart, decreaseQty, increaseQty, deleteCartItem } = useContext(CartContext);
     const prices = cart.map((product) => product.price * product.qty);
     let totalPrice = 0
     if (prices.length) totalPrice = prices.reduce((total, p) => total + p);
 
-    const handleDeleteCartItem = (id) => {
-        console.log(id)
-        deleteCartItem(id);
-    }
     return (
         <>
-            <LogoAndNavigation inActive="inActive"/>
+            <LogoAndNavigation inActive="inActive" />
             <Container>
                 {cart.length > 0 ? (
-                    <Table id="cart" className="table table-hover table-responsive">
+                    <Table id="cart" className="table table-hover">
                         <thead>
                             <tr>
-                                <th style={{ width: '50%' }}>Product</th>
-                                <th style={{ width: '10%' }}>Price</th>
-                                <th style={{ width: '8%' }}>Quantity</th>
+                                <th style={{ width: '38%' }}>Product</th>
+                                <th style={{ width: '10%' }} className="text-center">Price</th>
+                                <th style={{ width: '10%' }} className="text-center">Quantity</th>
+                                <th style={{ width: '10%' }} className="text-center">Itemleft</th>
                                 <th style={{ width: '22%' }} className="text-center">Subtotal</th>
                                 <th style={{ width: '10%' }}></th>
                             </tr>
@@ -42,23 +39,28 @@ export const Cart = () => {
                                                     </Col>
                                                     <Col sm={8}>
                                                         <h4 className="nomargin">{item.name}</h4>
-                                                        {/* <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p> */}
                                                     </Col>
-                                                    {/* <div class="col-sm-10">
-										<h4 class="nomargin">Product 1</h4>
-										<p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
-									</div> */}
+                                                    {/* <div class="col-sm-10"></div> */}
                                                 </Row>
                                             </td>
-                                            <td data-th="Price">${item.price}</td>
-                                            <td data-th="Quantity">
-                                                {/* <input type="number" className="form-control text-center" value="1" /> */}
-                                                <p>{item.qty}</p>
+                                            <td data-th="Price" className="text-center">${item.price}</td>
+                                            {item.itemLeft === 0 ? <td data-th="Quantity" className="text-center">
+                                                <button  onClick={() => decreaseQty(item.id)}> - </button>
+                                                <p className="d-inline pl-1 pr-1">{item.qty}</p>
+                                                <button disabled={true} onClick={() => increaseQty(item.id)}> + </button>
+                                            </td> :
+                                            <td data-th="Quantity" className="text-center">
+                                                <button onClick={() => decreaseQty(item.id)}> - </button>
+                                                <p className="d-inline pl-1 pr-1">{item.qty}</p>
+                                                <button onClick={() => increaseQty(item.id)}> + </button>
+                                            </td>
+                                            }
+                                            <td data-th="ItemLeft" className="text-center">
+                                               <p>{item.itemLeft}</p> 
                                             </td>
                                             <td data-th="Subtotal" className="text-center">${item.subTotal}</td>
                                             <td>
-                                                {/* <button className="btn btn-info btn-sm"><i className="fa fa-refresh"></i></button> */}
-                                                <button className="btn btn-danger btn-sm" onClick={() => { handleDeleteCartItem(item.id) }}><i className="fa fa-trash-o"></i></button>
+                                                <button className="btn btn-danger btn-sm" onClick={() => deleteCartItem(item.id)}><i className="fa fa-trash-o"></i></button>
                                             </td>
                                         </tr>
                                     )
@@ -79,7 +81,7 @@ export const Cart = () => {
                     </Table>
 
                 ) : (
-                        <div style={{color: "red" ,textAlign: "center", marginTop:"25%"}}>
+                        <div style={{ color: "red", textAlign: "center", marginTop: "25%" }}>
                             <h1>Cart is empty</h1>
                         </div>
                     )
